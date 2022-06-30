b1cc792fca7fdbf8f43d489f28c86b5825c3b302
Michael Aydinbas <michael.aydinbas@exxeta.com>
2022-06-30 17:11:44 +0200

    update michas code

--- data.py
+++ data.py
@@ -1,33 +1,40 @@
 from lib2to3.pgen2.pgen import DFAState
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.datasets import make_blobs, make_moons, make_circles
-
 import streamlit as st
+from sklearn.datasets import make_blobs, make_circles, make_moons
+from sklearn.model_selection import train_test_split
 
 
-def get_isolated_data(noise: float):
+@st.cache
+def get_isolated_data(noise: float) -> tuple[np.ndarray, np.ndarray]:
     X, y = make_blobs(n_samples=100, n_features=2, centers=np.array([[-2, 2], [2, -2]]), cluster_std=np.array([noise*1.5, noise*1.3]))
     return X, y
 
-
-def get_xor_data(noise: float):
+@st.cache
+def get_xor_data(noise: float) -> tuple[np.ndarray, np.ndarray]:
     X1, _ = make_blobs(n_samples=50, n_features=2, centers=np.array([[-2, 2], [2, -2]]), cluster_std=np.array([noise*1.5, noise*1.3]))
     y1 = np.zeros(X1.shape[0])
     X2, _ = make_blobs(n_samples=50, n_features=2, centers=np.array([[-2, -2], [2, 2]]), cluster_std=np.array([noise*1.4, noise*1.2]))
     y2 = np.ones(X2.shape[0])
     return np.r_[X1, X2], np.r_[y1, y2]
 
-
-def get_moon_data(noise: float):
+@st.cache
+def get_moon_data(noise: float) -> tuple[np.ndarray, np.ndarray]:
     X, y = make_moons(n_samples=100, noise=noise/5)
     return X, y
 
-
-def get_circle_data(noise: float):
+@st.cache
+def get_circle_data(noise: float) -> tuple[np.ndarray, np.ndarray]:
     X, y = make_circles(n_samples=100, noise=noise/5, factor=0.1)
     return X, y
+
+@st.cache
+def get_train_test_data(X: np.ndarray, y: np.ndarray, data_ratio: float) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=data_ratio)
+    return X_train, X_test, y_train, y_test
 
 
 DATASETS = {
@@ -36,17 +43,6 @@ DATASETS = {
     "Moon dataset": get_moon_data,
     "Circle dataset": get_circle_data,
 }
-
-
-def create_figure(X_train, X_test, y_train, y_test, ax):
-    lines = ax.plot(X_train[y_train==0, 0], X_train[y_train==0, 1], marker="o", markersize=10, linestyle="None", label="train")
-    first_color = lines[0].get_color()
-    lines = ax.plot(X_train[y_train==1, 0], X_train[y_train==1, 1], marker="o", markersize=10, linestyle="None")
-    second_color = lines[0].get_color()
-    
-    ax.plot(X_test[y_test==0, 0], X_test[y_test==0, 1], color=first_color, marker="x", markersize=15, linestyle="None", label="test")
-    ax.plot(X_test[y_test==1, 0], X_test[y_test==1, 1], color=second_color, marker="x", markersize=15, linewidth=2, linestyle="None")
-    
     
 
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import streamlit as st
+from plot import create_dataset_figure
 from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 
-from data import (DATASETS, create_figure, get_circle_data, get_isolated_data,
-                  get_moon_data, get_xor_data)
+from data import DATASETS, get_train_test_data
 from models import model_dict
 
 mpl.style.use("default")
@@ -15,30 +14,27 @@ mpl.style.use("default")
 st.set_page_config(layout="wide")
 st.title("Exxplore - Machine Learning Visualized")
 
-left, center, right = st.columns([25,50,25])
+left, right = st.columns([25,75])
 
 with left:
     st.subheader("Choose your dataset")
     
-    data_noise = st.slider("Select noise", min_value=0.0, max_value=1.0, step=0.05, value=0.3)
-    data_ratio = st.slider("Select train/test ratio", min_value=0.0, max_value=1.0, step=0.05, value=0.8)
+    data_noise = st.slider("Select noise", min_value=0.05, max_value=1.0, step=0.05, value=0.3)
+    data_ratio = st.slider("Select train/test ratio", min_value=0.1, max_value=0.9, step=0.05, value=0.8)
     
     dataset_name = st.selectbox("Select dataset", options=DATASETS.keys())
     data_func = DATASETS[dataset_name]
     fig, ax = plt.subplots(1, 1)
     
     X, y = data_func(data_noise)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=data_ratio)
-    create_figure(X_train, X_test, y_train, y_test, ax)
-    plt.axis('tight')
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(visible=True, axis="both")
-    plt.legend(loc="best")
-    st.pyplot(fig, clear_figure=True)
+    X_train, X_test, y_train, y_test = get_train_test_data(X, y, data_ratio)
+    
+    data_fig = create_dataset_figure(X_train, X_test, y_train, y_test)
+    
+    st.pyplot(data_fig, clear_figure=True)
 
 
-with center:
+with right:
     st.subheader("Classifier")
     paramter_values = {}
     for model_name in model_dict:
@@ -57,10 +53,14 @@ with center:
         model_dict[model_name]["fitted_estimator"] = model_dict[model_name]["estimator"](**paramter_values).fit(X_train, y_train)
         score = model_dict[model_name]["fitted_estimator"].score(X_test, y_test)
         st.write(f"score is: {score}")
-    
 
-with right:
-    model = LogisticRegression()
+
+    
+    
+    
+    
+    
+    
     
 
     
